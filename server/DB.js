c048a8e90972988d80054698408e676193942b55
Amit Saha <2105049@ugrad.cse.buet.ac.bd>
2024-01-18 09:24:47 +0600

    DB.js

--- server/DB.js
+++ server/DB.js
@@ -2,12 +2,21 @@ import pkg from "pg";
 const { Pool : POOL} = pkg;
 
 
+// const pool = new POOL({
+//     user: "postgres",
+//     host: "localhost",
+//     database: "nexusmeds",
+//     password: "1234",
+//     port: 5432,
+// });
+
 const pool = new POOL({
     user: "postgres",
     host: "localhost",
-    database: "nexusmeds",
+    database: "NexusMeds",
     password: "1234",
     port: 5432,
 });
+
 
 export default pool;
